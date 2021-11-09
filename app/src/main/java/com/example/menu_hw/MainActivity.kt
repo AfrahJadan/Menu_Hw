01c3a29849041776c3391684af27b84afadc3bd7
Afrah <Afrah.suliman.ruh.k@tuwaiq.edu.sa>
2021-11-09 22:07:30 +0300

    Edit on Switch(login - Sign out)

--- app/src/main/java/com/example/menu_hw/MainActivity.kt
+++ app/src/main/java/com/example/menu_hw/MainActivity.kt
@@ -1,35 +1,61 @@
-package com.example.menu_hw
+    package com.example.menu_hw
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
+    import android.content.Intent
+    import androidx.appcompat.app.AppCompatActivity
+    import android.os.Bundle
+    import android.util.Log
+    import android.view.Menu
+    import android.view.MenuItem
+    import androidx.recyclerview.widget.GridLayoutManager
+    import androidx.recyclerview.widget.LinearLayoutManager
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    class MainActivity : AppCompatActivity() {
+        private var islogin = true
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
 
-    }
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
-R.id.Set ->{
-    val intent=Intent(this,Setting::class.java)
-    startActivity(intent)
-
-}
-            R.id.Log ->{
-                item.title ="log out"
-            }
-            else -> super.onOptionsItemSelected(item)
         }
-return true
+        override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+            menuInflater.inflate(R.menu.menu, menu)
+            return true
+        }
+
+        override fun onOptionsItemSelected(item: MenuItem): Boolean {
+            when(item.itemId){
+                R.id.Contact_us->{
+                    return true
+                }
+                R.id.Log->{
+                    return true
+                }
+                R.id.sign_out->{
+                    return true
+                }
+    R.id.Set -> {
+        val intent = Intent(this, Setting::class.java)
+        startActivity(intent)
+    }
+                else ->
+                super.onOptionsItemSelected(item) }
+    return true }
+
+
+        override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
+           val login =menu?.findItem(R.id.Log)
+            val signout = menu?.findItem(R.id.sign_out)
+    if(islogin) {
+        islogin =!islogin
+        login?.setVisible(true)
+        signout?.setVisible(false)
+    } else {
+        islogin =!islogin
+        login?.setVisible(false)
+        signout?.setVisible(true)
+    }
+            return true
+    }
 
     }
-}
+
+
